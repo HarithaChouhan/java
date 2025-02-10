27c6f69d31cfc06d1c6120778e285eb452d527aa
HarithaChouhan <harithachouhan9@gmail.com>
2025-02-10 15:52:56 +0530

    Add files via upload

---
+++ AdditionExample.java.c
@@ -0,0 +1,15 @@
+improt java.until.Scanner;//
+#include<stdio.h>
+class AdditionExample
+{
+static void addation(int num1,int num2);
+{
+System.out.println(num1+num2);
+}
+Scannerscaner=new
+Scanner(System.in);
+addition(scanner.nextlnint(),scanner.nextint());
+addition(scanner.nextlnint(),scanner.nextint());
+addition(scanner.nextlnint(),scanner.nextint());
+}
+}
